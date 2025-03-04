15e800e646f56d5afb6a36a3fa266c2c5fedeb23
Artur <djajikamail@gmail.com>
2025-03-04 14:58:00 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+// import Image from 'next/image';
+import {Htag} from '@/components';
+
+export default function Home() {
+  return (
+    <>
+      <Htag tag='h1'>Курсы по Photoshop</Htag>
+    </>
+  );
+}
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import {FlatCompat} from '@eslint/eslintrc';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +11,29 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ),
+  {
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'quotes': ['error', 'single'],
+      'semi': 'warn',
+      '@typescript-eslint/no-empty-interface': [
+        'error',
+        { allowSingleExtends: true }
+      ]
+    },
+  },
+];
 
 export default eslintConfig;
