c2f1899338cc56c43e8c2c3880559852a0485beb
Artur <djajikamail@gmail.com>
2025-04-03 14:23:42 +0300

    add product styles

---
+++ components/Divider/Divider.props.ts
@@ -0,0 +1,3 @@
+import React  from 'react';
+
+export type DividerProps = React.ComponentProps<'hr'>
--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,7 +1,8 @@
 import {ProductProps} from './Product.props';
 import styles from './Product.module.css';
 import cn from 'classnames';
-import {Button, Card, Rating, Tag} from '@/components';
+import {Button, Card, Divider, Rating, Tag} from '@/components';
+import {priceUa} from '@/helpers/helpers';
 
 export const Product = ({product, className, ...props}: ProductProps) => {
 	return (
@@ -10,33 +11,42 @@ export const Product = ({product, className, ...props}: ProductProps) => {
 				<img src={product.image} alt={product.title}/>
 			</div>
 			<div className={styles.title}>{product.title}</div>
-			<div className={styles.price}>{product.price}</div>
-			<div className={styles.credit}>{product.credit}</div>
+			<div className={styles.price}>
+				{priceUa(product.price)}
+				{product.oldPrice && <Tag className={styles.discount} color='green'>{priceUa(product.price - product.oldPrice)}</Tag>}
+			</div>
+			<div className={styles.credit}>
+				{priceUa(product.credit)}/<span>мес</span>
+			</div>
 			<div className={styles.rating}>
 				<Rating rating={product.reviewAvg ?? product.initialRating}/>
 			</div>
 			<div className={styles.tags}>
 				{product.categories.map((category) => (
-					<Tag key={category} color='ghost'>{category}</Tag>
+					<Tag className={styles.category} key={category} color='ghost'>{category}</Tag>
 				))}
 			</div>
 			<div className={styles.priceTitle}>цена</div>
 			<div className={styles.creditTitle}>в кредит</div>
 			<div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
-			<div className={styles.hr}><hr/></div>
+			<Divider className={styles.hr}/>
 			<div className={styles.description}>{product.description}</div>
 			<div className={styles.features}>Фичи</div>
 			<div className={styles.advBlock}>
-				<div className={styles.advatntages}>
-					<div>Преимущества</div>
-					<div>{product.advantages}</div>
-				</div>
-				<div className={styles.disadvantages}>
-					<div>Недостатки</div>
-					<div>{product.disadvantages}</div>
-				</div>
+				{product.advantages &&
+					<div className={styles.advatntages}>
+						<div className={styles.advTitle}>Преимущества</div>
+						<div>{product.advantages}</div>
+					</div>
+				}
+				{product.disadvantages &&
+					<div className={styles.disadvantages}>
+						<div className={styles.advTitle}>Недостатки</div>
+						<div>{product.disadvantages}</div>
+					</div>
+				}
 			</div>
-			<div className={styles.hr}><hr/></div>
+			<Divider className={styles.hr}/>
 			<div className={styles.actions}>
 				<Button appearance='primary'>Узнать подробнее</Button>
 				<Button appearance='ghost' arrow='right'>Читать отзывы</Button>
