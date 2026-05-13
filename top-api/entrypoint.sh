09183530abbce2b2f9f7de257de3fb32d7920186
Artur Frolov <djajikamail@gmail.com>
2026-05-13 17:55:09 +0300

    Remove unused uploads logic from entrypoint script and Dockerfile.

--- top-api/entrypoint.sh
+++ top-api/entrypoint.sh
@@ -7,9 +7,5 @@ if [ ! -f /data/data.db ]; then
 	cp /app/data.db /data/data.db
 fi
 
-if [ -d /app/uploads ] && [ -z "$(find /data/uploads -mindepth 1 -maxdepth 1 2>/dev/null)" ]; then
-	cp -R /app/uploads/. /data/uploads/
-fi
-
 cd /data
 exec /app/top-api-linux-amd64
